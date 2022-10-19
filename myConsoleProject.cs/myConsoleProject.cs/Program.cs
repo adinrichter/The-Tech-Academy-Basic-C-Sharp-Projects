a67bf4b8d1005aba70c5c1ff8d42f866e461fe39
adinrichter <47506271+adinrichter@users.noreply.github.com>
2022-10-19 15:46:18 -0700

    Updated program to meet requirements

--- myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -1,38 +1,112 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO;
+
+// defines global variables
+public static class Global
+{
+    // response boolean for while loops
+    public static bool response;
+
+    // output to be used in loops
+    public static int page;
+    public static bool needsHelp;
+    public static int hours;
+}
+
 class Program
 {
     static void Main()
     {
-        // Sets the name of the program
+        // defines local variables
+        string name;
+        string course;
+        string positive;
+        string feedback;
+
+        // sets the name of the console window
         Console.Title = "Tech Academy Course Report";
         
-        // Gets user input
-        Console.WriteLine("What is your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine("\nWhat course are you on: ");
-        string course = Console.ReadLine();
-        Console.WriteLine("\nWhat page number are you on: ");
-        string pagenumber = Console.ReadLine();
-        Console.WriteLine("\nDo you need help with anything: ");
-        string help = Console.ReadLine();
-        Console.WriteLine("\nWere there any positive experiences you'd like to share: ");
-        string positive = Console.ReadLine();
-        Console.WriteLine("\nIs there any other feedback you'd like to provide: ");
-        string feedback = Console.ReadLine();
-        Console.WriteLine("\nHow many hours did you study today: ");
-        string hours = Console.ReadLine();
-        Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day! (press Enter to close)");
-        Console.ReadLine();
+        // gets user's full name
+        Console.WriteLine("What is your full name?");
+        name = Console.ReadLine();
 
-        // Compiles responses into one string
-        string parsedResponses = "Name: " + name + "\nCourse: " + course + "\nPage Number: " + pagenumber + "\nHelp: " + help + "\nPositive: " + positive + "\nFeedback: " + feedback + "\nHours Studied: " + hours + "\n\n";
+        // gets course name
+        Console.WriteLine("\nWhat course are you on?");
+        course = Console.ReadLine();
+        
+        // Gets user's page number and verifies that it is a number
+        Console.WriteLine("\nWhat page number are you on? (must be a number)");
+        Global.response = false;
+        while (Global.response == false)
+        {
+            try
+            {
+                Global.page = Convert.ToInt32(Console.ReadLine());
+                Global.response = true;
+            }
+            catch
+            {
+                Console.WriteLine("\nThe response must be a number\nWhat page number are you on?");
+            }
+        }
 
-        // Gets past responses written
+        // gets true/false response to whether or not the user needs help
+        Console.WriteLine("\nDo you need help with anything? (y/n)");
+        Global.response = false;
+        while (Global.response == false)
+        {
+            switch (Console.ReadLine())
+            {
+                case "y": Global.needsHelp = true; Global.response = true; break;
+                case "n": Global.needsHelp = false; Global.response = true; ; break;
+                default: Console.WriteLine("\nPlease enter 'y' or 'n'\nDo you need help with anything?"); break;
+            }
+        }
+        
+        // gets user's positive feedback
+        Console.WriteLine("\nWere there any positive experiences you'd like to share?");
+        positive = Console.ReadLine();
+
+        // gets additional feedback
+        Console.WriteLine("\nIs there any other feedback you'd like to provide?");
+        feedback = Console.ReadLine();
+
+        // gets number of hours spent on the course
+        Console.WriteLine("\nHow many hours did you study today? (must be a number)");
+        Global.response = false;
+        while (Global.response == false)
+        {
+            try
+            {
+                Global.hours = Convert.ToInt16(Console.ReadLine());
+                Global.response = true;
+            }
+            catch
+            {
+                Console.WriteLine("\nThe response must be a number\nHow many hours did you study today?");
+            }
+        }
+
+        // tells user that the report is being saved
+        Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+        // compiles responses into one string
+        string parsedResponses = 
+            "Name: " + name + 
+            "\nCourse Name: " + course + 
+            "\nPage Number: " + Global.page + 
+            "\nDoes the user need help: " + Global.needsHelp + 
+            "\nPositive: " + positive + 
+            "\nFeedback: " + feedback + 
+            "\nHours Studied: " + Global.hours + 
+            "\n\n";
+
+        // gets past responses
         string currentResponses = File.ReadAllText("./reports.txt");
 
-        // Adds new responses to previously written ones and writes them
-        // This could theoretically cause issues since I'm overwriting the entire file, but I don't care
+        // adds new responses to previously written ones and writes them
+        // this could theoretically cause issues if corruption occurs but I don't care
         File.WriteAllText("./reports.txt", currentResponses + parsedResponses);
     }
 }
