4aef21b07628248f6fa0ac8ab48235fe975f19a2
adinrichter <47506271+adinrichter@users.noreply.github.com>
2022-10-19 14:11:52 -0700

    Added comments and changed program title name

--- myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -4,9 +4,11 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Tech Academy Course Report");
+        // Sets the name of the program
+        Console.Title = "Tech Academy Course Report";
         
-        Console.WriteLine("\nWhat is your name: ");
+        // Gets user input
+        Console.WriteLine("What is your name: ");
         string name = Console.ReadLine();
         Console.WriteLine("\nWhat course are you on: ");
         string course = Console.ReadLine();
@@ -23,9 +25,14 @@ class Program
         Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day! (press Enter to close)");
         Console.ReadLine();
 
+        // Compiles responses into one string
         string parsedResponses = "Name: " + name + "\nCourse: " + course + "\nPage Number: " + pagenumber + "\nHelp: " + help + "\nPositive: " + positive + "\nFeedback: " + feedback + "\nHours Studied: " + hours + "\n\n";
+
+        // Gets past responses written
         string currentResponses = File.ReadAllText("./reports.txt");
 
+        // Adds new responses to previously written ones and writes them
+        // This could theoretically cause issues since I'm overwriting the entire file, but I don't care
         File.WriteAllText("./reports.txt", currentResponses + parsedResponses);
     }
 }
