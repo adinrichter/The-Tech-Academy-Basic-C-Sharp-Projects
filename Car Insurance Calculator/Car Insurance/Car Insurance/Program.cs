edf9dda1af80d5956fd3d32f2a37568a95545217
adinrichter <47506271+adinrichter@users.noreply.github.com>
2022-10-20 15:19:45 -0700

    Created car insurance program

---
+++ Car Insurance Calculator/Car Insurance/Car Insurance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Car_Insurance
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Car Insurance Approver";
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (response must be \"true\"/\"false\")");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && dui == false && tickets <= 3);
+            Console.ReadLine();
+        }
+    }
+}
