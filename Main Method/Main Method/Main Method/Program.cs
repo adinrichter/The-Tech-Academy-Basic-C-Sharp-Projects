80b10b15309f4e94b0f783ee6f8a83626f5139f5
adinrichter <47506271+adinrichter@users.noreply.github.com>
2022-10-28 21:19:31 -0700

    Added Main Method assignment

---
+++ Main Method/Main Method/Main Method/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // calls the different methods with different functions based on input type
+            Console.WriteLine(Math.Operate(8));
+            Console.WriteLine(Math.Operate(8.0m));
+            Console.WriteLine(Math.Operate("8"));
+            Console.ReadLine();
+        }
+    }
+    public class Math
+    {
+        // takes in an integer and returns double it
+        public static int Operate(int number)
+        {
+            int result = number * 2;
+            return result; 
+        }
+        // takes in a decimal and returns half of it
+        public static decimal Operate(decimal number)
+        {
+            decimal result = number / 2;
+            return result;
+        }
+        // takes in a string and returns the string + 2
+        public static int Operate(string number)
+        {
+            int result = Convert.ToInt32(number) + 2;
+            return result;
+        }
+    }
+}
