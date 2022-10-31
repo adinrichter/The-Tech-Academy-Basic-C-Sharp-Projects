48c6575aa382b68f6dc8fc36b1abff745950d962
adinrichter <47506271+adinrichter@users.noreply.github.com>
2022-10-31 13:33:34 -0700

    updated

---
+++ Main Method/Main Method 3/Main Method 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Math.Operation(1, 2);
+            Math.Operation(in1: 8, in2: 41);
+            Console.ReadLine();
+        }
+        public class Math
+        {
+            public static void Operation(int in1, int in2)
+            {
+                string result = in1 * 2 + " " + in2;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
