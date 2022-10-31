bfcd1bb131579945587bd1058d1edc24382c7c36
adinrichter <47506271+adinrichter@users.noreply.github.com>
2022-10-31 11:31:11 -0700

    Added main method 2

---
+++ Main Method/Main Method 2/Main Method 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // the exponent function returns the number squared by default
+            Console.WriteLine("20 squared is: " + Math.Exponent(20f));
+            // the exponent function returns the any desired exponent if given in the argument
+            Console.WriteLine("20 cubed is: " + Math.Exponent(20f, 3));
+            Console.WriteLine("20 to the power 6 is: " + Math.Exponent(20f, 6));
+            Console.ReadLine();
+        }
+        public class Math
+        {
+            public static float Exponent(float num, float exp = 2)
+            {
+                // multiplies the number by itself the number of times specified by the exponent
+                float result = num;
+                for (float i = 1; i < exp; i++)
+                {
+                    result *= num;
+                }
+                return result;
+            }
+
+        }
+    }
+}
